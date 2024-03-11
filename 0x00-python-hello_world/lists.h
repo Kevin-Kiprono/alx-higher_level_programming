25a7160e9c2a1e2b1eeef1cce99679a920c06dde
Kevin-Kiprono <kevoopronoh2003@gmail.com>
2024-03-12 02:22:27 +0300

    Hello World

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,17 +5,17 @@
 
 /**
  * struct listint_s - singly linked lists
- * @n: interger 
+ * @n: interger
  * @next: point to the next node.
  *
- * description: singly linked list node structures 
+ * description: singly linked list node structures
  *
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} 
+}
 listint_t;
 
 site_t print_listint(const listint_t *h);
