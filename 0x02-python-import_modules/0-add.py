5f17ebbd423726285129c3a380596cbf753f8b97
Kevin Kiprono <kevoopronoh2003@gmail.com>
2024-03-17 04:19:57 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-if  __name__    ==  "__main__":
+
+if __name__ == "__main__":
 
     """print the sum of 1 and 2."""
 
-    #imports the add function from the module add_0
+    from add_0 import add
 
-from add_0 import add
- 
-     #assigning values of variables a and b
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-     #calling the add function and storing the result      
-
-result = add(a, b)
-
-     #printing the results in the required format
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
 
