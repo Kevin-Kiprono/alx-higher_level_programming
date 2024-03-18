6587b46bfb3c2f8e3a0c728ccd7c04d80dcfea6a
Kevin Kiprono <kevoopronoh2003@gmail.com>
2024-03-18 14:56:06 -0700

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -10,8 +10,8 @@ def element_at(my_list,idx):
     """
     if my_list:
         if idx < 0:
-            return
-        elif idx > len(my_list) - 1:
-            return
+            return None
+        elif idx >= len(my_list):
+            return None
         else:
             return my_list[idx]
